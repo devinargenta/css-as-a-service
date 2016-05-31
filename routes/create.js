faa53dcd5f2269b591f029645244a0c2afb11fcb
Devin Argenta <devin@devinargenta.com>
2016-05-30 21:29:25 -0400

    holler if u handle hex

--- routes/create.js
+++ routes/create.js
@@ -9,18 +9,14 @@ router.get('/:property/:value', function(req, res, next) {
 
   if ( property.length === value.length ) {
 
-
     for (var i = 0; i < property.length; i++ ){
-        style[property[i]] = value[i];
+        style[property[i]] = value[i].replace(/#?([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})/g, '#$1$2$3');
       }
+
     } else {
 
       res.send(400, `yo shit don't match`);
 
-    }
-
-    for ( var key in style ) {
-      style[key].replace(/#?([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})/g, '#$1$2$3');
     }
 
     res.send(style);
